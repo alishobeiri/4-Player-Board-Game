1d1db39f9a6f7db71a8bab31964153bf1c1bdf1e
Ali Shobeiri <alishobeiri@ymail.com>
2017-02-17 21:47:33 -0500

    Added Controller Package

--- src/ca/mcgill/ecse223/tileo/application/TileOApplication.java
+++ src/ca/mcgill/ecse223/tileo/application/TileOApplication.java
@@ -1,11 +1,25 @@
 package ca.mcgill.ecse223.tileo.application;
 
+import ca.mcgill.ecse223.tileo.model.Game;
 import ca.mcgill.ecse223.tileo.model.TileO;
 
 public class TileOApplication {
 	private static TileO tileO;
 	
 	public static void main(String args[]){
+
 		
+	}
+	
+	public static Game getGame(){
+		return tileO.getCurrentGame();
+	}
+	
+	public static void save() {
+		//Use to save
+	}
+	
+	public static void load() {
+		//Use to load
 	}
 }
---
+++ src/ca/mcgill/ecse223/tileo/controller/TileOController.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse223.tileo.controller;
+
+public class TileOController {
+	//Add controller methods that you need to the file 
+}
