6d1f13612541035b29c4d22ed3081ab010b507a5
Ali Shobeiri <alishobeiri@ymail.com>
2017-02-17 21:16:37 -0500

    Added Application Package and Class

---
+++ src/ca/mcgill/ecse223/tileo/application/TileOApplication.java
@@ -0,0 +1,11 @@
+package ca.mcgill.ecse223.tileo.application;
+
+import ca.mcgill.ecse223.tileo.model.TileO;
+
+public class TileOApplication {
+	private static TileO tileO;
+	
+	public static void main(String args[]){
+		
+	}
+}
