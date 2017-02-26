05122644a57c0fda3760d256d0444f242d4dd65d
Ali Shobeiri <alishobeiri@ymail.com>
2017-02-26 11:09:34 -0500

    createActionTile method changed and changed createStartTile to assignStartingTile

--- src/ca/mcgill/ecse223/tileo/controller/TileOController.java
+++ src/ca/mcgill/ecse223/tileo/controller/TileOController.java
@@ -15,28 +15,34 @@ public class TileOController {
 	// Add controller methods that you need to the file
 	public void createNewActionTile(int x, int y, int numTurns) throws InvalidInputException{
 		Game game=TileOApplication.getCurrentGame();
-		try{
-			new ActionTile(x, y, game, numTurns);
-		}catch(RuntimeException e){
-			throw new InvalidInputException("Error");
+		List<Tile> list=game.getTiles();
+		for(Tile tile: list){
+			if(tile.getX()==x && tile.getY()==y){
+				throw new InvalidInputException("A tile already exists in that position.");
+			}
 		}
+		game.setMode(Mode.DESIGN);
+		Tile action = new ActionTile(x, y, game, numTurns);
 	}
 
-	public void createStartingTile(int x, int y, int playerNumber) throws Exception {
+	public void assignStartingTile(int x, int y, int playerNumber) throws Exception {
 		Game game=TileOApplication.getCurrentGame();
-		int totalPlayers=game.numberOfPlayers();
-		if(totalPlayers<playerNumber && playerNumber<4){
-			game.addPlayer(new Player(playerNumber, game));
-			totalPlayers=game.numberOfPlayers();
-		}else if(playerNumber>4){ //Could remove this later down the line
-			throw new InvalidInputException("Player number is invalid");
+		Player player;
+		try{
+			player=game.getPlayer(playerNumber-1);
+		}catch(Exception e){
+			throw new InvalidInputException("Player does not exist or might not have been initialized");
 		}
-		Player player = game.getPlayer(totalPlayers-1);
-		if(player.hasStartingTile()){
-			throw new InvalidInputException("Player already has starting tile");
-		}else{
-			player.setStartingTile(new NormalTile(x, y, game));
+		boolean found=false;
+		for(Tile tile : game.getTiles()){
+			if(tile.getX()==x && tile.getY()==y){
+				found=true;
+				player.setStartingTile(tile);
+			}
 		}
+		if(!found){
+			throw new InvalidInputException("The tile could not be found");
+		}		
 	}
 
 	// Action Card Methods
